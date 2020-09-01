22dc4b218a56557ea5dd10007c4d2afe6d54f70f
Bruno Silva <bruno3dcontato@gmail.com>
2020-09-01 09:23:22 -0300

    feat: added opcional param to space trim

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports = function expand(obj, deleteRawKey = true, separator = ",") {
+module.exports = function expand(obj, deleteRawKey = true, trimSpaces = true, separator = ",") {
     Object.keys(obj).forEach((key) => {
         const subkeys = key.split(separator);
         const value = obj[key];
 
         subkeys.forEach((subkey) => {
-            obj[subkey.trim()] = value;
+            obj[trimSpaces ? subkey.trim() : subkey] = value;
         });
 
         if (deleteRawKey) {
